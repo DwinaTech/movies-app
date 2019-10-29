a560c3a609e8121ae5036133c65d99ad7bf2dbd8
dwinatech <diwina1993@hotmail.com>
2019-10-29 23:42:49 +0000

    Add ability to add a filem to favorite moves list

--- src/components/Moves/MoveCard.js
+++ src/components/Moves/MoveCard.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 
-const MoveCard = ({ title, actor, release, image }) => {
+const MoveCard = ({ title, actor, release, image, isFavorite, onClick }) => {
   return (
     <Card style={{ width: "18rem" }}>
       <Card.Img variant="top" src={image} alt={title} />
@@ -9,7 +9,12 @@ const MoveCard = ({ title, actor, release, image }) => {
         <Card.Title>{title}</Card.Title>
         <Card.Text>{release}</Card.Text>
         <Card.Text>{actor}</Card.Text>
+        <Card.Text></Card.Text>
       </Card.Body>
+      <Card.Footer onClick={onClick}>
+        <i className="fas fa-heart" style={{ color: isFavorite ? 'red' : 'black' }}></i>
+        <small className="text-muted"> Press to add to favorite list</small>
+      </Card.Footer>
     </Card>
   );
 };
--- src/components/Moves/index.js
+++ src/components/Moves/index.js
@@ -12,6 +12,28 @@ class index extends Component {
       actors: Object.keys(data || {})
     };
   }
+
+  handleFavorite = (e, moveTitle) => {
+    e.preventDefault();
+    const { moves, actors } = this.state;
+
+    const updatedMoves = actors.map(actor => {
+      return {
+        [actor]: moves[actor].map(move => {
+          if (move["Film"] === moveTitle) {
+            return {
+              ...move,
+              isFavorite: true
+            };
+          }
+          return move;
+        })
+      }
+    });
+
+    this.setState({ moves: updatedMoves[0] });
+  };
+
   render() {
     const { moves, actors } = this.state;
 
@@ -23,13 +45,17 @@ class index extends Component {
             moves[actor].map(move => {
               const newProps = {
                 title: move["Film"],
-                actor: move["Bond Actor"],
                 image: move.ImageURL,
+                actor: move["Bond Actor"],
+                isFavorite: move.isFavorite,
                 release: move["UK release date"]
               };
               return (
                 <Col key={move["Film"]} xs={12} sm={12} md={4}>
-                  <MoveCard {...newProps} />
+                  <MoveCard
+                    onClick={e => this.handleFavorite(e, move["Film"])}
+                    {...newProps}
+                  />
                 </Col>
               );
             })
