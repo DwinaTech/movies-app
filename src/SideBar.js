d9b552998c875c86c82793ce3b19bf2e9adaf506
dwinatech <diwina1993@hotmail.com>
2019-10-29 22:58:02 +0000

    Update the sideBar to handle active key correctly

--- src/SideBar.js
+++ src/SideBar.js
@@ -1,33 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Nav } from "react-bootstrap";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const SideBar = () => {
-  const [activeKey, setActiveKey] = useState(1);
-  const handleClick = (e, newActiveKey) => {
-      e.preventDefault();
-      console.log('newActiveKey ====>>>>', newActiveKey);
-      
-    setActiveKey(newActiveKey);
-  };
+  const { pathname } = window.location;
+  const [activeKey, setActiveKey] = useState(pathname);
+
+  useEffect(() => {
+    setActiveKey(pathname);
+  }, [pathname]);
 
   return (
     <Nav variant="pills" className="flex-column" defaultActiveKey={activeKey}>
       <Nav.Item>
         <Nav.Link disabled>Moves:</Nav.Link>
       </Nav.Item>
-      <Nav.Item onClick={e => handleClick(e, 1)}>
-        <Nav.Link eventKey="1" as={Link} to="/">
+      <Nav.Item>
+        <Nav.Link eventKey="/" as={Link} to="/">
           Home
         </Nav.Link>
       </Nav.Item>
-      <Nav.Item onClick={e => handleClick(e, 2)}>
-        <Nav.Link eventKey="2" as={Link} to="/moves">
+      <Nav.Item>
+        <Nav.Link eventKey="/moves" as={Link} to="/moves">
           Moves
         </Nav.Link>
       </Nav.Item>
-      <Nav.Item onClick={e => handleClick(e, 3)}>
-        <Nav.Link eventKey="3" as={Link} to="/favorite">
+      <Nav.Item>
+        <Nav.Link eventKey="/favorite" as={Link} to="/favorite">
           Favorite
         </Nav.Link>
       </Nav.Item>
