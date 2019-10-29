bc07fe6a8b575b5502d296090eb19ff2a3dd125c
dwinatech <diwina1993@hotmail.com>
2019-10-29 21:34:35 +0000

    Add react router and build the initial layout

---
+++ src/Routes.js
@@ -0,0 +1,29 @@
+import React from "react";
+import SideBar from "./SideBar";
+import { Container, Row, Col } from "react-bootstrap";
+import { Route, BrowserRouter as Router } from "react-router-dom";
+import "./routes.css";
+
+function Routes() {
+  return (
+    <Router>
+      <Container fluid>
+        <Row>
+          <Col className="side-bar" xs={3}>
+            <SideBar />
+          </Col>
+          <Col className="main-contents" xs={9}>
+            <Route exact path="/" render={() => <h1>Home component</h1>} />
+            <Route path="/moves" render={() => <h1>Moves component</h1>} />
+            <Route
+              path="/favorite"
+              render={() => <h1>Favorite component</h1>}
+            />
+          </Col>
+        </Row>
+      </Container>
+    </Router>
+  );
+}
+
+export default Routes;
