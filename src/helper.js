fc2a048ea2f2f85556a9e0db6332855210a266d5
dwinatech <diwina1993@hotmail.com>
2019-10-31 00:43:42 +0000

    Refactoring helpers and add favorite functionlities

--- src/helper.js
+++ src/helper.js
@@ -13,39 +13,85 @@ const months = [
   { December: "12" }
 ];
 
-const findMonth = monthInString => {    
-    const result = months.find(month => month[monthInString]);
-    const keys = Object.keys(result || {})
-    const formatedMonth = result[keys[0]];
-    return formatedMonth;
-}
-
-export const formatDate = date => {
-    const originalDate = date.split(" ");
-    const month = findMonth(originalDate[1]);
-    const formatedDate = `${originalDate[2]}-${month}-${originalDate[0]}`
-    return formatedDate;
+const findMonth = monthInString => {
+  const result = months.find(month => month[monthInString]);
+  const keys = Object.keys(result || {});
+  const formatedMonth = result[keys[0]];
+  return formatedMonth;
 };
 
-export const updateFavorite = ({moveTitle, moves, actors}) => {
-    return actors.map(actor => {
-      return {
-        [actor]: moves[actor].map(move => {
-          if (move["Film"] === moveTitle && !move.isFavorite) {
-            return {
-              ...move,
-              isFavorite: true
-            };
-          }
-          if (move["Film"] === moveTitle && move.isFavorite) {
-            return {
-              ...move,
-              isFavorite: false
-            };
-          }
-          return move;
-        })
-      }
-    });
-  };
+export const formatDate = date => {
+  const originalDate = date.split(" ");
+  const month = findMonth(originalDate[1]);
+  const formatedDate = `${originalDate[2]}-${month}-${originalDate[0]}`;
+  return formatedDate;
+};
 
+export const updateFavorite = (moveTitle, currentActor) => {
+  let movesDate = {};
+  const moves = getFilmsData();
+  for (let actor in moves) {
+    if (actor === currentActor) {
+      const films = moves[actor].map(actor => {
+        if (actor["Film"] === moveTitle && !actor.isFavorite) {
+          return {
+            ...actor,
+            isFavorite: true
+          };
+        }
+        if (actor["Film"] === moveTitle && actor.isFavorite) {
+          return {
+            ...actor,
+            isFavorite: false
+          };
+        }
+        return actor;
+      });
+      movesDate = { ...movesDate, [actor]: films };
+    } else {
+      movesDate = { ...movesDate, [actor]: moves[actor] };
+    }
+  }
+  return movesDate;
+};
+
+export const storeFilmsData = data => {
+  return Promise.resolve(
+    localStorage.setItem("getFilmsData", JSON.stringify(data))
+  );
+};
+
+export const getFilmsData = () => {
+  const films = localStorage.getItem("getFilmsData");
+  const moves = films ? JSON.parse(films) : {};
+  return moves;
+};
+
+export const addDateToFilms = () => {
+  let movesDate = {};
+  const moves = getFilmsData();
+  for (let move in moves) {
+    const films = moves[move].map(move => {
+      return {
+        ...move,
+        date: formatDate(move["UK release date"])
+      };
+    });
+    movesDate = { ...movesDate, [move]: films };
+  }
+  return movesDate;
+};
+
+export const getActorsData = () => {
+  return Object.keys(getFilmsData() || {});
+};
+
+export const getFavoriteFilms = () => {
+    let movesDate = {};
+    const moves = getFilmsData();
+    for (let move in moves) {
+      const films = moves[move].filter(move => move.isFavorite);
+      movesDate = { ...movesDate, [move]: films };
+    }
+    return movesDate;
+  };
