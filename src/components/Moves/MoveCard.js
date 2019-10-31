655890a7ac9bae10932d99e01f2a3b86d62447eb
dwinatech <diwina1993@hotmail.com>
2019-10-31 21:53:27 +0000

    Show MoveModal when press a move card image

--- src/components/Moves/MoveCard.js
+++ src/components/Moves/MoveCard.js
@@ -1,10 +1,19 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 
-const MoveCard = ({ title, actor, release, image, isFavorite, onClick }) => {
+const MoveCard = props => {
+  const {
+    title,
+    actor,
+    release,
+    image,
+    isFavorite,
+    onClick,
+    onShowModal
+  } = props;
   return (
     <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src={image} alt={title} />
+      <Card.Img onClick={onShowModal} variant="top" src={image} alt={title} />
       <Card.Body>
         <Card.Title>{title}</Card.Title>
         <Card.Text>{release}</Card.Text>
@@ -12,8 +21,13 @@ const MoveCard = ({ title, actor, release, image, isFavorite, onClick }) => {
         <Card.Text></Card.Text>
       </Card.Body>
       <Card.Footer onClick={onClick}>
-        <i className="fa fa-heart" style={{ color: isFavorite ? 'red' : 'black' }}></i>
-        <small className="text-muted">{` Press to ${isFavorite ? 'remove from' : 'add to'} favorite list`}</small>
+        <i
+          className="fa fa-heart"
+          style={{ color: isFavorite ? "red" : "black" }}
+        ></i>
+        <small className="text-muted">{` Press to ${
+          isFavorite ? "remove from" : "add to"
+        } favorite list`}</small>
       </Card.Footer>
     </Card>
   );
