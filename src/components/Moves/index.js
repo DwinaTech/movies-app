436c487f2aa772cc0d1d00854729c03a4af9b41c
dwinatech <diwina1993@hotmail.com>
2019-11-03 14:46:53 +0000

    Add search by lead actor name functionalities and UI

--- src/components/Moves/index.js
+++ src/components/Moves/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import MoveCard from "./MoveCard.js";
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Form } from "react-bootstrap";
 import {
   markMoveAsFavorite,
   storeMovesData,
@@ -16,7 +16,8 @@ class Moves extends Component {
     this.state = {
       moves: [],
       actors: [],
-      showModal: false
+      showModal: false,
+      leadActor: ''
     };
   }
 
@@ -45,48 +46,71 @@ class Moves extends Component {
     this.setState({ showModal: true, moveDate });
   };
 
+  filterMoves = (moves = []) => {
+    const { leadActor } = this.state;
+    if (leadActor) {
+      return moves.length && moves.filter(move => {
+        const actor = move['Bond Actor'].toLowerCase();
+        return actor.includes(leadActor.toLowerCase());
+      });
+    }
+    return moves;
+  };
+
+  handleSearchChange = e => {
+    e.preventDefault();
+    this.setState({ leadActor: e.target.value })
+  }
+
   render() {
     const { moves, actors, showModal, moveDate } = this.state;
     return (
-      <Row className="moves">
-        <MoveModal {...moveDate} />
-        {actors.map(
-          actor =>
-            actor &&
-            moves[actor].map(move => {
-              const newProps = {
-                title: move["Film"],
-                image: move.ImageURL,
-                actor: move["Bond Actor"],
-                release: move["UK release date"]
-              };
-              const modalProps = {
-                ...newProps,
-                show: showModal,
-                onHide: this.onHideModal,
-                description: move["Description"],
-                actor: move["Bond Actor"]
-              };
-              const cardProps = {
-                ...newProps,
-                onShowModal: () =>
-                  this.onShowModal({ ...newProps, ...modalProps }),
-                isFavorite: move.isFavorite,
-                onClick: event =>
-                  this.handleFavorite({
-                    event,
-                    actor,
-                    id: move.id
-                  })
-              };
-              return (
-                <Col key={move.id} xs={12} sm={12} md={4}>
-                  <MoveCard {...cardProps} />
-                </Col>
-              );
-            })
-        )}
-      </Row>
+      <div className="moves">
+        <Row>
+          <Col xs={12} className="search">
+            <Form.Control onChange={this.handleSearchChange} name="leadActor" placeholder="Search by Lead actor" />
+          </Col>
+        </Row>
+        <Row>
+          <MoveModal {...moveDate} />
+          {actors.map(
+            actor =>
+              actor &&
+              this.filterMoves(moves[actor]).map(move => {
+                const newProps = {
+                  title: move["Film"],
+                  image: move.ImageURL,
+                  actor: move["Bond Actor"],
+                  release: move["UK release date"]
+                };
+                const modalProps = {
+                  ...newProps,
+                  show: showModal,
+                  onHide: this.onHideModal,
+                  description: move["Description"],
+                  actor: move["Bond Actor"]
+                };
+                const cardProps = {
+                  ...newProps,
+                  onShowModal: () =>
+                    this.onShowModal({ ...newProps, ...modalProps }),
+                  isFavorite: move.isFavorite,
+                  onClick: event =>
+                    this.handleFavorite({
+                      event,
+                      actor,
+                      id: move.id
+                    })
+                };
+                return (
+                  <Col key={move.id} xs={12} sm={12} md={4}>
+                    <MoveCard {...cardProps} />
+                  </Col>
+                );
+              })
+          )}
+        </Row>
+      </div>
     );
   }
 }
